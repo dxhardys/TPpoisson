d1435797e9b51dbdc68f4616b854c816e5f7caad
Arhab Sofiane <arhabsofiane123@gmail.com>
2024-01-06 14:53:44 +0100

    Implemented richardson_alpha_opt() function

--- part2/TP_Poisson_C_students_2022_part2/src/lib_poisson1D_richardson.c
+++ part2/TP_Poisson_C_students_2022_part2/src/lib_poisson1D_richardson.c
@@ -20,7 +20,8 @@ double eigmin_poisson1D(int *la){
 }
 
 double richardson_alpha_opt(int *la){
-  return 0;
+  double opt = eigmax_poisson1D(la) + eigmin_poisson1D(la);
+  return (2/opt);
 }
 
 void richardson_alpha(double *AB, double *RHS, double *X, double *alpha_rich, int *lab, int *la,int *ku, int*kl, double *tol, int *maxit, double *resvec, int *nbite){
